f49f038f629bd5deb5adce5a14872a49e9e50570
Sumant Kataria <sumant.kataria@gmail.com>
2021-03-06 17:26:44 +0530

    added file

---
+++ Problem_Solving_Patterns/Are_There_Duplicates.js
@@ -0,0 +1,9 @@
+/**
+ * Multiple Pointers Pattern/Set
+ * areThereDuplicates(1, 2, 3) // false
+ * areThereDuplicates(1, 2, 2) // true
+ * areThereDuplicates('a', 'b', 'c' , 'a') // true
+ */
+const areThereDuplicates = (...numbers) => {
+    return (new Set(numbers) !== numbers.length)
+  }
