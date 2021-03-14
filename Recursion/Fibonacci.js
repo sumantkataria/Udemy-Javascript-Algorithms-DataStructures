89c13059bfed48bc71acd552400afca1021a3ab7
Sumant Kataria <sumant.kataria@gmail.com>
2021-03-14 12:10:01 +0530

    added file

---
+++ Recursion/Fibonacci.js
@@ -0,0 +1,10 @@
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
+
+function fib(num, sum, count = 1, first = 0, second = 1) {
+    if(num === count) return second
+    sum = first + second; first = second; second = sum 
+    return fib(num, sum, ++count, first, second)
+}
