f2cc60df6f14e5474fc2b9b076c65b4801ab2c2e
Sumant Kataria <sumant.kataria@gmail.com>
2021-03-19 01:24:40 +0530

    added insertion sort

---
+++ Sorting_Algorithms/InsertionSort.js
@@ -0,0 +1,12 @@
+const insertionSort = (arr) => {
+
+    let len = arr.length, min
+    for(let i=0; i<len; i++) {
+        min = i
+        for(let j=i+1; j<len; j++) {
+            if(arr[j] < arr[min]) min = j
+        }
+        [arr[i], arr[min]] = [arr[min], arr[i]]
+    }
+    return arr
+}
