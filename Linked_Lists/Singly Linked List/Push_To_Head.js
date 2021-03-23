8a2d5fa67af85384b1a74f4570aeea2e2f0ad2c1
Sumant Kataria <sumant.kataria@gmail.com>
2021-03-23 14:34:31 +0530

    added file

---
+++ Linked_Lists/Singly Linked List/Push_To_Head.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(val, next) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    pushToHead (val) {
+        let newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode; this.tail = newNode
+        } else {
+            newNode.next = this.head
+            this.head = newNode;
+        }
+        this.length++
+        return this
+    }
+    
+}
+
+let list = new SinglyLinkedList()
+list.pushToHead('Hi')
+list.pushToHead('Hello')
+list.pushToHead('Good')
+list.pushToHead('Bye')
