a74c200549fa5ba88de39202b8d570393ed38f76
Sumant Kataria <sumant.kataria@gmail.com>
2021-03-24 12:00:10 +0530

    added file

---
+++ Linked_Lists/Singly Linked List/Set_Node_By_Index.js
@@ -0,0 +1,52 @@
+class Node {
+    constructor(val, next) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    pushToHead (val) {
+        let newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode; this.tail = newNode
+        } else {
+            newNode.next = this.head
+            this.head = newNode;
+        }
+        this.length++
+        return this
+    }
+
+    getNodeByIndex (index) {
+        if(index < 0 || index >= this.length) return null
+        let currentNode = this.head, counter = 0 
+        while(counter !== index) {
+            currentNode = currentNode.next;counter++
+        }
+        return currentNode
+    }
+
+    setNodeByIndex (index, value) {
+        let node = this.getNodeByIndex(index)
+        if(node) {
+            node.val = value
+            return true
+        }
+        return false
+    }
+    
+}
+
+let list = new SinglyLinkedList()
+list.pushToHead('Hi')
+list.pushToHead('Hello')
+list.pushToHead('Good')
+list.pushToHead('Bye')
+list.setNodeByIndex(2, 'updated')
