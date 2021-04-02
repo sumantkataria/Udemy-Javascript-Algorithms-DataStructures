2d5dfa23a8e5f5c1016ea34a86e099aae8f53f7e
Sumant Kataria <sumant.kataria@gmail.com>
2021-04-03 01:02:16 +0530

    added file

--- Hash_Tables(Hash Map)/Hash_Function_Get.js
+++ Hash_Tables(Hash Map)/Hash_Function_Get.js
@@ -13,13 +13,24 @@ class HashTable {
       }
       return total;
     }
-
     set(key,value){
       let index = this._hash(key);
       if(!this.keyMap[index]){
         this.keyMap[index] = [];
       }
       this.keyMap[index].push([key, value]);
+    }
+
+    get(key){
+      let index = this._hash(key);
+      if(this.keyMap[index]){
+        for(let i = 0; i < this.keyMap[index].length; i++){
+          if(this.keyMap[index][i][0] === key) {
+            return this.keyMap[index][i][1]
+          }
+        }
+      }
+      return undefined;
     }
 }
   
@@ -31,3 +42,4 @@ ht.set('salmon','#FA8072')
 ht.set('lightcoral','#F08080')
 ht.set('mediumvioletred','#C71585')
 ht.set('plum','#DDA0DD')
+ht.get('plum')
